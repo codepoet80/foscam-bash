db6a0828829493911288535c7534d7417be7f5e7
codepoet80 <jon.wise@jonandnic.com>
2023-07-29 12:21:54 -0400

    update sample ffmpeg command

--- update-cams.sh
+++ update-cams.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 declare -a cams=(
-	"ffmpeg -y -i rtsp://camuser:campassword@192.168.1.XXX:88/videoMain -vframes 1 "
+	"ffmpeg -y -rtsp_transport tcp -i rtsp://camuser:campassword@192.168.1.XXX:88/videoMain -frames:v 1 "
 )
 
 camLength=${#cams[@]}
