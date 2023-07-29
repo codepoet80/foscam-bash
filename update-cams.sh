cd2f112867eb8e3b18affe071fe17ce4602a02c1
codepoet80 <jon.wise@jonandnic.com>
2023-07-29 12:46:02 -0400

    update script with current dir

--- update-cams.sh
+++ update-cams.sh
@@ -3,9 +3,10 @@ declare -a cams=(
 	"ffmpeg -y -rtsp_transport tcp -i rtsp://camuser:campassword@192.168.1.XXX:88/videoMain -frames:v 1 "
 )
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 camLength=${#cams[@]}
 for (( i=0; i<${camLength}; i++ ));
 do
 	echo "Updating Camera #$i..."
-	${cams[$i]} web/cam-$i.jpg
+	${cams[$i]} $SCRIPT_DIR/web/cam-$i.jpg
 done
