e4ff8577ef5f43b5822ddb89d83d5e1e1947c78f
codepoet80 <jon.wise@jonandnic.com>
2023-07-29 12:54:07 -0400

    offset default update rate

--- web/config.php
+++ web/config.php
@@ -1,6 +1,6 @@
 <?php
 $config=array(
-	'refresh' => 60000,
+	'refresh' => 30000,
 //	Cameras will get default names based on their filename
 //		You can optionally override this behavior by setting up a name array as below
 //	'cam_names' => array(
