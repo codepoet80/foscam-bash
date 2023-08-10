95c3f566686149e96de3b45ae6456927cc4d844c
codepoet <jon.wise@jonandnic.com>
2023-08-10 14:07:45 -0400

    Update update-cams.sh

--- update-cams.sh
+++ update-cams.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 declare -a cams=(
-	"ffmpeg -y -rtsp_transport tcp -i rtsp://camuser:campassword@192.168.1.XXX:88/videoMain -frames:v 1 "
+	"ffmpeg -y -rtsp_transport tcp -i rtsp://camuser:campassword@192.168.1.XXX:88/videoMain -frames:v 1 "	#FoscamHD
+ 	"wget -O --user camuser --password campassword http://192.168.1.XXX:88/snapshot.cgi"	#FoscamSD
 )
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
