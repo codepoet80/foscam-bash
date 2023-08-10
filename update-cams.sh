34b8dda167affdd2ab90d04fc11ff1db785adf8f
codepoet <jon.wise@jonandnic.com>
2023-08-10 14:21:47 -0400

    Update update-cams.sh

--- update-cams.sh
+++ update-cams.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 declare -a cams=(
-	"ffmpeg -y -rtsp_transport tcp -i rtsp://camuser:campassword@192.168.1.XXX:88/videoMain -frames:v 1 "	#FoscamHD
- 	"wget -O --user camuser --password campassword http://192.168.1.XXX:88/snapshot.cgi"	#FoscamSD
+	"ffmpeg -y -rtsp_transport tcp -i rtsp://camuser:campassword@192.168.1.XXX:88/videoMain -frames:v 1 CAMING"	#FoscamHD
+ 	"wget -O CAMING --user camuser --password campassword http://192.168.1.XXX:88/snapshot.cgi"	#FoscamSD
 )
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 camLength=${#cams[@]}
 for (( i=0; i<${camLength}; i++ ));
 do
-	echo "Updating Camera #$i..."
-	${cams[$i]} $SCRIPT_DIR/web/temp-$i.jpg
-	mv $SCRIPT_DIR/web/temp-$i.jpg $SCRIPT_DIR/web/cam-$i.jpg
+        echo "Updating Camera #$i..."
+        outFile=web/cam-$i.jpg
+        command=${cams[$i]}
+        command=${command/CAMIMG/"$outFile"}
+        $command
 done
