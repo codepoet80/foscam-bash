82d8c7f318b116b0a1c9068f2b15947c05baca24
codepoet <info@codepoetry.app>
2023-11-18 17:49:01 -0500

    use systemd

--- update-cams.sh
+++ update-cams.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
-if [ ! -f ./config.sh ]; then
-	echo "Config file not found! Unable to proceed..."
-	echo "Copy config-example.sh to config.sh, and set the URLs to your cameras within it"
-	exit
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+if [ ! -f $SCRIPT_DIR/config.sh ]; then
+        echo "Config file not found! Unable to proceed..."
+        echo "Copy config-example.sh to config.sh, and set the URLs to your cameras within it"
+        exit
 else
-	echo "Updating cams..."
-	source config.sh
-	SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-	camLength=${#cams[@]}
-	for (( i=0; i<${camLength}; i++ ));
-	do
-		tempFile=$SCRIPT_DIR/temp-$i.jpg
-		outFile=$SCRIPT_DIR/web/cam-$i.jpg
-		command=${cams[$i]}
-		command=${command/CAMIMG/"$tempFile"}
-		$command
-		mv $tempFile $outFile
-	done
+        source $SCRIPT_DIR/config.sh
+        camLength=${#cams[@]}
+        echo "Updating $camLength cams..."
+        for (( i=0; i<${camLength}; i++ ));
+        do
+                tempFile=$(mktemp).jpg
+                outFile=$SCRIPT_DIR/web/cam-$i.jpg
+                echo
+                echo "Saving to: $outFile"
+                echo
+                command=${cams[$i]}
+                command=${command/CAMIMG/"$tempFile"}
+                $command
+                mv -f $tempFile $outFile
+        done
 fi
