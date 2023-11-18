19dd63a5f3b0acbaeced2d0154893417ee09e2f2
codepoet <info@codepoetry.app>
2023-11-18 14:48:04 -0500

    better config, better error handling

--- update-cams.sh
+++ update-cams.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-declare -a cams=(
-	"ffmpeg -y -rtsp_transport tcp -i rtsp://camuser:campassword@192.168.1.XXX:88/videoMain -frames:v 1 CAMING"	#FoscamHD
- 	"wget -O CAMING --user camuser --password campassword http://192.168.1.XXX:88/snapshot.cgi"	#FoscamSD
-)
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-camLength=${#cams[@]}
-for (( i=0; i<${camLength}; i++ ));
-do
-	tempFile=$SCRIPT_DIR/temp-$i.jpg
-	outFile=$SCRIPT_DIR/web/cam-$i.jpg
-	command=${cams[$i]}
-	command=${command/CAMIMG/"$tempFile"}
-	$command
- 	mv $tempFile $outFile
-done
+if [ ! -f ./config.sh ]; then
+	echo "Config file not found! Unable to proceed..."
+	echo "Copy config-example.sh to config.sh, and set the URLs to your cameras within it"
+	exit
+else
+	echo "Updating cams..."
+	source config.sh
+	SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+	camLength=${#cams[@]}
+	for (( i=0; i<${camLength}; i++ ));
+	do
+		tempFile=$SCRIPT_DIR/temp-$i.jpg
+		outFile=$SCRIPT_DIR/web/cam-$i.jpg
+		command=${cams[$i]}
+		command=${command/CAMIMG/"$tempFile"}
+		$command
+		mv $tempFile $outFile
+	done
+fi
--- web/index.php
+++ web/index.php
@@ -4,32 +4,41 @@ include("config.php");
 <html>
     <head>
         <title>Foscam Cameras</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
         <link rel="stylesheet" href="style.css">
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
         <link rel="icon" href="favicon.ico" type="image/x-icon">
         <script>
         var rate = <?php echo $config['refresh'];?>;
-        function imgReady(id) {
-            console.log(id + " is ready!");
-            var spinner = document.getElementById("spinner-" + id);
+        function imgReady(self) {
+            console.log(self.id + " is ready!");
+            var spinner = document.getElementById("spinner-" + self.id);
             spinner.style.display = "none";
             setTimeout (
-                function(id) {
-                    console.log("timeout fired for " + id);
-                    var cam = document.getElementById(id);
-                    var spinner = document.getElementById("spinner-" + id);
+                function(self) {
+                    console.log("timeout fired for " + self.id);
+                    var cam = document.getElementById(self.id);
+                    var spinner = document.getElementById("spinner-" + self.id);
                     var d=new Date();
-                    cam.src = cam.id + ".jpg?time=" + d.getTime();
+                    cam.src = self.id + ".jpg?time=" + d.getTime();
                     spinner.style.display = "inline";
-                }, rate, id
-            )
+                }, rate, self
+            );
+        }
+        function camError(self) {
+            self.src='offline.png';
+            var spanToMove = document.getElementById("span-" + self.id);
+            var spans = document.getElementsByTagName("span");
+            spanToMove.parentNode.appendChild(spanToMove.parentNode.replaceChild(spans[spans.length-1], spanToMove));
         }
         </script>
     </head>
     <body>
+    <table width=100% height=100% border=0 cellpadding=0 cellspacing=0>
+    <tr>
+    <td width=100% height=100% align="center" valign="middle">
         <h1><img src="icon.png"><br>Foscam Cameras</h1>
-
-        <div class="row">
+        <div id="container">
         <?php
         $files = glob('*.{jpg}', GLOB_BRACE);
         $c = 0;
@@ -43,23 +52,25 @@ include("config.php");
                 }
             }
             ?>
-            <div class="column">
+            <span class="item" id="span-<?php echo $cam_id; ?>">
                 <p align="center">
-                    <span class="camera-name"><?php echo $cam_name; ?></span>
-		    <?php
-		    if (isset($config['cam_links']) && $config['cam_links'][$c] != "") {
-		    ?>
-		    <a href="<?php echo $config['cam_links'][$c]?>">
-		    <?php } else { echo "<a>"; } ?>
-                    <img class="camera" id="<?php echo $cam_id; ?>" onload="imgReady('<?php echo $cam_id ?>');" title="<?php echo $cam_name; ?>" src="<?php echo $this_img . '?time=' . hrtime(true); ?>">
-		    </a>
-                    <img class="spinner" id="spinner-<?php echo $cam_id ?>" src="spinner.gif">
+                    <p class="camera-name"><?php echo $cam_name; ?></p>
+                    <?php
+                    if (isset($config['cam_links']) && $config['cam_links'][$c] != "") {
+                    ?>
+                    <a href="<?php echo $config['cam_links'][$c]?>">
+                    <?php } else { echo "<a>"; } ?>
+                        <img class="camera" id="<?php echo $cam_id; ?>" onload="imgReady(this);" title="<?php echo $cam_name; ?>" src="<?php echo $this_img . '?time=' . hrtime(true); ?>" onerror="camError(this)">
+                    </a>
                 </p>
-
-            </div>
+                <img class="spinner" id="spinner-<?php echo $cam_id ?>" src="spinner.gif">
+            </span>
         <?php
             $c++;
         }
         ?>
+    </td>
+    </tr>
+    </table>
     </body>
 </html>
